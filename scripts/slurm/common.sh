0de73100ad23c2092cb8bd6c2f02a94a9d25c6a7
yhsure <andbje@hotmail.com>
2026-04-29 16:35:59 +0200

    Add reproducible latent ablation Slurm runs

--- scripts/slurm/common.sh
+++ scripts/slurm/common.sh
@@ -22,6 +22,7 @@ function drift_export_env() {
   export HF_HOME="${hf_home}"
   export HF_ROOT="${HF_HOME}"
   export HF_HUB_CACHE="${HF_HOME}/hub"
+  export HF_HUB_OFFLINE="${HF_HUB_OFFLINE:-1}"
   export DRIFT_SDVAE_PATH="${DRIFT_SDVAE_PATH:-${HF_HUB_CACHE}/models--stabilityai--sd-vae-ft-mse/snapshots/31f26fdeee1355a5c34592e401dd41e45d25a493}"
   export WANDB_MODE="${WANDB_MODE:-offline}"
   export WANDB_PROJECT="${WANDB_PROJECT:-drift}"
